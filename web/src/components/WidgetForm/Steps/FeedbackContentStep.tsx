9c6af6eca59457c77e2de05d7bd2e46b81558bc2
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-06-20 17:20:31 -0300

    screenshot wrong name

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -28,7 +28,7 @@ export function FeedbackContentStep({
     await api.post("/feedback", {
       type: feedbackType,
       comment,
-      screenShoot,
+      screenshot: screenShoot,
     });
     setIsSendingFeedback(false);
     onFeedbackSent();
