22b04c097707b4efc834ba0041f83e219d7bf3b6
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-06-20 08:19:08 -0300

    fix: problems solved

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -55,7 +55,7 @@ export function FeedbackContentStep({
           onChange={(event) => setComment(event.target.value)}
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md
           focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none
-          scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent " /* scrollbar-thin */
+          scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
         <footer className="flex  gap-2 mt-2">
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -49,7 +49,7 @@ export function WidgetForm() {
     setFeedbackSent(false);
   };
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto:">
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
         <FeedbackSuccessStep onFeedRestartRequested={handleRestartFeedback} />
       ) : (
