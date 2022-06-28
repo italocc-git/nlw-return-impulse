41d4c045b1538206b4df5a3b429919acbd3438c2
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-06-28 14:14:07 -0300

    fix: fixing erros in test

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,56 +1,72 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
 const createFeedbackSpy = jest.fn() /* spy function */
+const getFeedbackSpy = jest.fn()
 const sendMailSpy = jest.fn()
 
 const submitFeedback = new SubmitFeedbackUseCase(
-    { create: createFeedbackSpy},
-    { sendMail: sendMailSpy}
+    { create: createFeedbackSpy, getAll: getFeedbackSpy },
+    { sendMail: sendMailSpy }
 )
 
 
 
-describe('Submit feedback' , () => {
-    it('should be able to submit a feedback', async() => {
-        
+describe('Submit feedback', () => {
+    it('should be able to submit a feedback', async () => {
+
 
         await expect(submitFeedback.execute({
             type: 'BUG',
             comment: 'example comment',
-            screenshot: 'data:image/png;base64,auau2878sxsadb'
+            screenshot: 'data:image/png;base64,auau2878sxsadb',
+            userId: '123'
         })).resolves.not.toThrow()
 
         expect(createFeedbackSpy).toHaveBeenCalled()
         expect(sendMailSpy).toHaveBeenCalled()
     })
 
-    it('should not be able to submit feedback without type', async() => {
-        
+    it('should not be able to submit feedback without type', async () => {
+
 
         await expect(submitFeedback.execute({
             type: '',
             comment: 'example comment',
-            screenshot: 'data:image/png;base64,auau2878sxsadb'
+            screenshot: 'data:image/png;base64,auau2878sxsadb',
+            userId: '123'
         })).rejects.toThrow()
     })
 
-    it('should not be able to submit feedback without comment', async() => {
-        
+    it('should not be able to submit feedback without comment', async () => {
+
 
         await expect(submitFeedback.execute({
             type: 'BUG',
             comment: '',
-            screenshot: 'data:image/png;base64,auau2878sxsadb'
+            screenshot: 'data:image/png;base64,auau2878sxsadb',
+            userId: '123'
         })).rejects.toThrow()
     })
 
-    it('should not be able to submit feedback with an invalid screenshot', async() => {
-        
+    it('should not be able to submit feedback without userId', async () => {
+
+
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,auau2878sxsadb',
+            userId: ''
+        })).rejects.toThrow()
+    })
+
+    it('should not be able to submit feedback with an invalid screenshot', async () => {
+
 
         await expect(submitFeedback.execute({
             type: 'BUG',
             comment: 'comment test',
-            screenshot: 'test.jpg'
+            screenshot: 'test.jpg',
+            userId: '123'
         })).rejects.toThrow()
     })
 })
