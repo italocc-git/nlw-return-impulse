a79200e8b9fbc9a9a162bb0a5310e9fb3b379a9c
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-07-08 11:00:22 -0300

    fix: resolvendo o problema das requisições de feedbacks por usuário

--- web/src/components/Dashboard/services/index.ts
+++ web/src/components/Dashboard/services/index.ts
@@ -1,11 +1,13 @@
 import { api } from "../../../services/api"
 
 async function getFeedbackByUser(userId: string) {
-    return await api.get('feedbacksUser', {
-        data: {
-            userId
+    return await api.get(`feedbacksUser/${userId}`,
+        {
+            params: {
+                userId
+            }
         }
-    })
+    )
 }
 
 export { getFeedbackByUser }
